dd1d68ec070fc2a3a0987d174a3dba190237e6b9
manognaniit <vvrlmanogna@gmail.com>
2016-09-10 22:51:10 +0530

    Add files via upload
    my project

---
+++ cart1/src/main/java/cart1/controller/HomeController.java
@@ -0,0 +1,18 @@
+package cart1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home(){
+		System.out.println("in home method");
+		return "home";
+	}
+	@RequestMapping("/signup")
+	public String signup(){
+		System.out.println("in signup method");
+		return "signup";
+	}
+
+}
